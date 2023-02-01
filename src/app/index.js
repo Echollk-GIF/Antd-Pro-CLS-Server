13429430980ab19923feb84186177ecc70dc80df
Frank <841290355@qq.com>
2023-02-02 07:30:36 +0800

    feat: 引入parameter

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,7 @@
 const Koa = require('koa')
 const KoaBody = require('koa-body')
+const parameter = require('koa-parameter')
+
 const errHandler = require('./errHandler')
 const app = new Koa()
 
@@ -7,6 +9,8 @@ const userRouter = require('../router/user.route')
 
 //KoaBody中间件要在所有路由之前
 app.use(KoaBody())
+//parameter中间件也要在所有路由之前
+app.use(parameter(app))
 app.use(userRouter.routes())
 
 // 统一的错误处理
