404bd2fb4b877f88c93799d77c333ecd6a9cadb6
Frank <841290355@qq.com>
2023-02-02 05:30:14 +0800

    feat: 合法性校验、合理性（有bug暂未解决）

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,9 +1,32 @@
-const { registerUserAccount } = require('../service/user.service')
+const { registerUserAccount, getUserInfo } = require('../service/user.service')
 
 class UserController {
   async registerAccount (ctx, next) {
     // 1. 获取数据
     const { username, password } = ctx.request.body
+
+    // 合法性
+    if (!username || !password) {
+      console.error('用户名或密码为空', ctx.request.body)
+      ctx.status = 400
+      ctx.body = {
+        code: '10001',
+        message: '用户名或密码为空',
+        result: '',
+      }
+      return
+    }
+    // 合理性
+    if (getUserInfo({ username })) {
+      ctx.status = 409
+      ctx.body = {
+        code: '10002',
+        message: '用户已经存在',
+        result: '',
+      }
+      return
+    }
+
     // 2. 操作数据库
     const res = await registerUserAccount(username, password)
     // 3. 返回结果
--- src/service/user.service.js
+++ src/service/user.service.js
@@ -6,6 +6,21 @@ class UserService {
     const res = await User.create({ username, password })
     return res.dataValues
   }
+  //获取用户信息
+  async getUserInfo ({ id, username, password, authority }) {
+    const whereOpt = {}
+
+    id && Object.assign(whereOpt, { id })
+    username && Object.assign(whereOpt, { username })
+    password && Object.assign(whereOpt, { password })
+    authority && Object.assign(whereOpt, { authority })
+
+    const res = await User.findOne({
+      attributes: ['id', 'username', 'password', 'authority'],
+      where: whereOpt,
+    })
+    return res ? res.dataValues : null
+  }
 }
 
 module.exports = new UserService()
