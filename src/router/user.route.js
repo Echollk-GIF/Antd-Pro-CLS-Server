7df0e759a2b70550bb55d4449da0f694a604e13f
Frank <841290355@qq.com>
2023-02-02 03:08:01 +0800

    feat: 拆分controller

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -1,10 +1,13 @@
 const Router = require('koa-router')
 
+const { registerAccount, loginAccount } = require('../controller/user.controller')
+
 const router = new Router({ prefix: '/user' })
 
-// GET /user/
-router.get('/', (ctx, next) => {
-  ctx.body = 'hello users'
-})
+// 账号密码注册接口
+router.post('/register/account', registerAccount)
+
+// 账号密码登录接口
+router.post('/login/account', loginAccount)
 
 module.exports = router
