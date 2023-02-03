38699bd0f259943148bbfa4f09c2d0d7861c981b
Frank <841290355@qq.com>
2023-02-03 23:46:26 +0800

    feat: 根据省份获取城市列表

--- src/controller/global.controller.js
+++ src/controller/global.controller.js
@@ -16,5 +16,22 @@ class globalController {
       ctx.app.emit('error', provinceAcquisitionFailure, ctx)
     }
   }
+  //根据省份获取城市列表
+  async getCityList (ctx, next) {
+    const provinceId = ctx.params.province
+    //1.获取数据
+    try {
+      const res = require('../mock/geographic/city.json')
+      //3.返回结果
+      ctx.body = {
+        code: 0,
+        message: '城市数据获取成功',
+        data: res[provinceId]
+      }
+    } catch (err) {
+      console.log(err)
+      ctx.app.emit('error', cityAcquisitionFailure, ctx)
+    }
+  }
 }
 module.exports = new globalController()
--- src/router/global.route.js
+++ src/router/global.route.js
@@ -1,9 +1,11 @@
 const Router = require('koa-router')
-const { getProvinceList } = require('../controller/global.controller')
+const { getProvinceList, getCityList } = require('../controller/global.controller')
 const router = new Router()
 
 //获取省份数据信息
 router.get('/geographic/province', getProvinceList)
+//根据省份获取城市列表
+router.get('/geographic/city/:province', getCityList)
 
 module.exports = router
 
